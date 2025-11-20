5ff6d0db563d0e104a8f667054ee981de3f0c568
ItstheTsu <AllanCorr3a@gmail.com>
2025-11-20 01:17:37 -0300

    Reset do repositório, aplicando .gitignore

--- backend/src/main/java/com/omnicron/mylist/repository/PasswordResetTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.omnicron.mylist.repository;
-
-import com.omnicron.mylist.entity.PasswordResetToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
-    PasswordResetToken findByToken(String token);
-}
